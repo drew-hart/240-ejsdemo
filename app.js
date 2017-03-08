0bb2e0e7c29eafaf0c96ea6794e4b52f074f163b
Drew Hart <drew@drewhart.com>
2017-03-08 15:55:54 -0600

    node app not includes views folder and view engine is now ejs

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static('public'));
+app.use(express.static('views/'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', (req, res) => {
   const thing = req.params.thing;
-  res.render('love.ejs', {thingVar: thing});
+  res.render('love', { thingVar: thing });
 });
 
 app.listen(8080);
