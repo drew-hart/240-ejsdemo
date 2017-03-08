5728feae46de3eefc0fafc33270a0c3273bbe092
Drew Hart <drew@drewhart.com>
2017-03-08 16:47:27 -0600

    testing post routes in app.js

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(express.static('views/'));
 app.set('view engine', 'ejs');
+
+const friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily'];
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.post('/addfriend', (req, res) => {
+  friends.push(req.body.name);
+  res.redirect('/friends');
+});
+
 app.get('/friends', (req, res) => {
-  res.render('friends');
+  res.render('friends', { friends: friends });
 });
 app.listen(8080);
