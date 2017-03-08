6cd79aa453793067745dff628493fc083cc2c4c9
Drew Hart <drew@drewhart.com>
2017-03-08 16:46:33 -0600

    added body parser to project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('views/'));
 app.set('view engine', 'ejs');
